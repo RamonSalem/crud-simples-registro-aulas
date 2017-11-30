d3c719409163a7aba4a34f02470e3b1fbc1c2c37
Ramon Salém <salemramon@gmail.com>
2017-11-30 16:03:54 -0200

    few changes and body-parser added

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const bodyParser= require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
+
 mongoose.connect('mongodb://localhost/registro_aulas', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
@@ -23,12 +29,12 @@ const testRegistro = {
 }
 
 const registro = new RegistrosModel(testRegistro);
-registro.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Criado');
-  }
+  registro.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Criado');
+    }
 });
 
 
@@ -36,8 +42,25 @@ app.get('/', (req, res)=> {
   //Retorna registros
 });
 
-app.post('/', (req, res)=> {
+app.post('/registros', (req, res)=> {
     //Cria registros
+    
+    const newRegistro = {
+      titulo : req.body.titulo,
+      atividades : req.body.atividades,
+      alunosPresentes : req.body.alunosPresentes,
+      data : Date.now()
+    };
+    
+    const registro = new RegistrosModel(newRegistro);
+
+    registro.save()
+    .then(result =>{
+      return res.status(200).json(result);
+    })
+    .catch(err=>{
+      return res.status(500).json(err);
+    });
     
     //res.send('Hello World');
  });
@@ -54,5 +77,5 @@ const server = app.listen(3000, function () {
    const host = server.address().address
    const port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Rodando em http://%s:%s", host, port)
 })
